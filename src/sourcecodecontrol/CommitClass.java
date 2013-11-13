44672c3f5e69a106b7fcc8540960cd295514e001
vskulic <vuk.skulic@gmail.com>
2013-11-13 01:22:31 -0500

    Trying to implement file opening, try/catch blocks

--- src/sourcecodecontrol/CommitClass.java
+++ src/sourcecodecontrol/CommitClass.java
@@ -12,10 +12,11 @@ public class CommitClass {
 		String confirm;
 
 		Scanner in = new Scanner(System.in);
-		System.out.println("Enter the name of the file you wish to commit (or 'exit'):");
+		System.out
+				.println("Enter the name of the file you wish to commit (or 'exit'):");
 		fileName = in.nextLine();
 
-		//keep getting filenames while the user did not enter 'exit'
+		// keep getting filenames while the user did not enter 'exit'
 		while (!fileName.equalsIgnoreCase("exit")) {
 			System.out.println("\nYou've entered: '" + fileName + "'.");
 			System.out.println("Is this correct? (y|n)");
@@ -23,14 +24,23 @@ public class CommitClass {
 
 			if (confirm.equalsIgnoreCase("y")) {
 				System.out.println("You said yes.");
-				
-				//insert try/catch block here to test validity of file name
-				//(read up on try/catch blocks)
-				
+
+				try {
+					//dont know what Im doing here
+					PrintWriter out = new PrintWriter(new FileWriter(fileName));
+				} catch (FileNotFoundException e) {
+					System.err.println("FileNotFoundException: "
+							+ e.getMessage());
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					System.err.println("Caught IOException: " + e.getMessage());
+				}
+
 			} else if (confirm.equalsIgnoreCase("n")) {
 				System.out.println("You said no.");
-				//allow user to re-enter filename
-				
+				// allow user to re-enter filename
+
 			}
 		}
 
