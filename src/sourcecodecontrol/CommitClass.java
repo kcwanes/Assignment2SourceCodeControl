e6d0939520c5aa322c26772fdc975daebd86a056
vskulic <vuk.skulic@gmail.com>
2013-11-12 23:10:41 -0500

    Testing user input. Get an error for some reason.

--- src/sourcecodecontrol/CommitClass.java
+++ src/sourcecodecontrol/CommitClass.java
@@ -6,8 +6,10 @@ import java.util.Scanner;
 import java.lang.String;
 
 public class CommitClass {
-	Scanner fileName = new Scanner (System.in);
-	String input = "";
-	//System.out.print("Enter the name of the file you wish to commit:");
-	//input = fileName.next();
+	String fileName;
+	
+	Scanner in = new Scanner (System.in);
+	
+	System.out.println("Enter the name of the file you wish to commit:");
+	fileName = in.nextLine();
 }
