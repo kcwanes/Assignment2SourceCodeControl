a254914292c917f22e061d5ced119069acf109bd
vskulic <vuk.skulic@gmail.com>
2013-11-12 23:03:41 -0500

    Basic print/user input statement

--- src/sourcecodecontrol/CommitClass.java
+++ src/sourcecodecontrol/CommitClass.java
@@ -1,9 +1,13 @@
 package sourcecodecontrol;
 
-
 import java.io.*;
 import java.sql.*;
+import java.util.Scanner;
+import java.lang.String;
 
 public class CommitClass {
-
+	Scanner fileName = new Scanner (System.in);
+	String input = "";
+	System.out.print("Enter the name of the file you wish to commit:");
+	input = fileName.next();
 }
