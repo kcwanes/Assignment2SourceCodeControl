281cfb8849b57d0d7decf213cc577234063d3bd0
vskulic <vuk.skulic@gmail.com>
2013-11-12 23:29:42 -0500

    Added missing key element - a method directly within the class (silly mistake not having it)

--- src/sourcecodecontrol/CommitClass.java
+++ src/sourcecodecontrol/CommitClass.java
@@ -6,10 +6,14 @@ import java.util.Scanner;
 import java.lang.String;
 
 public class CommitClass {
-	String fileName;
-	
-	Scanner in = new Scanner (System.in);
-	
-	System.out.println("Enter the name of the file you wish to commit:");
-	fileName = in.nextLine();
+
+	public void commit() {
+		String fileName;
+
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Enter the name of the file you wish to commit:");
+		fileName = in.nextLine();
+	}
+
 }
