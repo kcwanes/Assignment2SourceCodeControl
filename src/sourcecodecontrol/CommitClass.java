975375b147590689d35be480edabfcc837734dda
vskulic <vuk.skulic@gmail.com>
2013-11-13 00:15:31 -0500

    Some more basic functionality

--- src/sourcecodecontrol/CommitClass.java
+++ src/sourcecodecontrol/CommitClass.java
@@ -9,13 +9,32 @@ public class CommitClass {
 
 	public void commitMethod() {
 		String fileName;
+		String confirm;
 
 		Scanner in = new Scanner(System.in);
-
-		System.out.println("Enter the name of the file you wish to commit:");
+		System.out.println("Enter the name of the file you wish to commit (or 'exit'):");
 		fileName = in.nextLine();
-		System.out.println("\nYou've entered: '" + fileName + "'.");
-		System.out.println("Is this correct?");
+
+		//keep getting filenames while the user did not enter 'exit'
+		while (!fileName.equalsIgnoreCase("exit")) {
+			System.out.println("\nYou've entered: '" + fileName + "'.");
+			System.out.println("Is this correct? (y|n)");
+			confirm = in.nextLine();
+
+			if (confirm.equalsIgnoreCase("y")) {
+				System.out.println("You said yes.");
+				
+				//insert try/catch block here to test validity of file name
+				//(read up on try/catch blocks)
+				
+			} else if (confirm.equalsIgnoreCase("n")) {
+				System.out.println("You said no.");
+				//allow user to re-enter filename
+				
+			}
+		}
+
+		System.out.println("You have hit 'exit'.");
 
 	}
 
