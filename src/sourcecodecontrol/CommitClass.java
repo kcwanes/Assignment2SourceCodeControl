62023e7452dc839e2abe6021a6ac393e50ace10d
vskulic <vuk.skulic@gmail.com>
2013-11-17 16:58:44 -0500

    Big update -- added code to copy a user-specified input file, add a timestamp to it, and output it to a user-specified folder

--- src/sourcecodecontrol/CommitClass.java
+++ src/sourcecodecontrol/CommitClass.java
@@ -3,6 +3,7 @@ package sourcecodecontrol;
 import java.io.*;
 import java.nio.*;
 import java.sql.*;
+import java.util.Date;
 import java.util.Scanner;
 import java.lang.String;
 import java.lang.Object;
@@ -10,59 +11,103 @@ import java.lang.Object;
 public class CommitClass {
 	// Comment to test commit from desktop
 
-	public void commitMethod() {
+	public String appendTimeStamp(String file, String time) {
+		String extension = "";
+		int k = file.lastIndexOf('.');
+		
+		if (k > 0) {
+			extension = file.substring(k);
+		}
+
+		System.out.println("The extension of " + file + " is " + extension);
+		
+		file = file.replace(extension, "");
+		file = file + "-" + time + extension;
+		
+		System.out.println("new fileName = " + file);
+		
+		return file;
+	}
+	
+	public static void copyFile(final File src, final File dest)
+			throws IOException {
+		copyInputStrToFile(new FileInputStream(src), dest);
+		dest.setLastModified(src.lastModified());
+	}
+
+	public static void copyInputStrToFile(final InputStream in, final File dest)
+			throws IOException {
+		copyInputStrToOutputStr(in, new FileOutputStream(dest));
+	}
+
+	public static void copyInputStrToOutputStr(final InputStream in,
+			final OutputStream out) throws IOException {
+		try {
+			try {
+				final byte[] buffer = new byte[1024];
+				int n;
+				while ((n = in.read(buffer)) != -1)
+					out.write(buffer, 0, n);
+			} finally {
+				out.close();
+			}
+		} finally {
+			in.close();
+		}
+	}
+
+	public void commitMethod() throws IOException {
 		String fileName;
+		String path;
 		String confirm;
 
+		String pathWithFileName;
+
 		Scanner in = new Scanner(System.in);
+
 		System.out
-				.println("Enter the name of the file you wish to commit (or 'exit'):");
+				.println("Enter the filepath of the file you wish to commit:");
+		path = in.nextLine();
+
+		System.out.println("Enter the name of the file you wish to commit:");
 		fileName = in.nextLine();
-		// keep getting filenames while the user did not enter 'exit'
-		// while (!fileName.equalsIgnoreCase("exit")) {
-		System.out.println("\nYou've entered: '" + fileName + "'.");
+
+		pathWithFileName = path + "\\" + fileName;
+
+		System.out.println("\nThe file is: ");
+		System.out.println(pathWithFileName);
+
 		System.out.println("Is this correct? (y|n)");
 		confirm = in.nextLine();
 
 		if (confirm.equalsIgnoreCase("y")) {
 			System.out.println("You said yes.");
 
-			try {
-				System.out.println("\nAgain, You've entered: '" + fileName
-						+ "'.");
+			Date date = new Date();
+			System.out.println(new Timestamp(date.getTime()));
 
-				// ideally, object inFile can read text from the user specified
-				// file, fileName
+			String theTime = date.toString();
 
-				// FOR NOW this is hardcoded to file 'file.txt' since I cant
-				// actually get
-				// it to work for a user specified file... yet
-				FileReader reader = new FileReader(
-						"/Users/vuk/Assignment2SourceCodeControl/src/sourcecodecontrol/file.txt");
-				Scanner inFile = new Scanner(reader);
+			System.out.println(theTime);
+			theTime = theTime.replace(' ', '-');
+			theTime = theTime.replace(':', '-');
 
-				// create a file for writing to. hard-code it to
-				// 'fileOutput.txt' for now
-				PrintWriter out = new PrintWriter(
-						"/Users/vuk/Assignment2SourceCodeControl/src/sourcecodecontrol/fileOutput.txt");
+			String newFile = appendTimeStamp(fileName, theTime);
+			System.out.println("newFile = " + newFile);
+			
+			String newFilePath = path + "\\" + newFile;
+			System.out.println("newFilePath = " + newFilePath);
+			
+			File src = new File(pathWithFileName);
 
-				// while there is data to read in the input file inFile
-				while (inFile.hasNextLine()) {
-					String line = inFile.nextLine();
-					System.out.println(line);
-					out.println(line);
-					// KNOWN ISSUES:
-					// -output file will have a new line appended to the
-					// end even if the input file did not have one
-					// -apostrophes and possible other characters do
-					// not get copied properly
-				}
-
-				out.close();
-			} catch (IOException exception) {
-				exception.printStackTrace();
-				System.out.println("Can't find that file!");
-			}
+			newFilePath = newFilePath.replace("\\", "\\\\"); 
+			System.out.println("newFilePath = " + newFilePath);
+	
+			File dest = new File(newFilePath);			
+			//dest1.mkdirs();
+			dest.createNewFile();
+			
+			copyFile(src, dest);
 
 		} else if (confirm.equalsIgnoreCase("n")) {
 			System.out.println("You said no.");
